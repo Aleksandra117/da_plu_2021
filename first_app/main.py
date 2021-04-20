74e2f3a0c7654d8ae37d52da941ca4637fd8bfc7
Aleksandra117 <ola.adamska10@gmail.com>
2021-04-20 13:17:13 +0200

    Task 1.4

--- first_app/main.py
+++ first_app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import datetime
+from datetime import timedelta
 
 app = FastAPI()
+app.patient_id = 1
 
 @app.get("/")
 def root():
@@ -26,3 +30,27 @@ def method():
 def method():
 	return {"method": "DELETE"}
 
+class Patient(BaseModel):
+	id: Optional[int] = None
+	name:str
+	surname:str
+	register_date: Optional[int] = None
+	vaccination_date: Optional[int] = None
+	
+@app.post('/register', status_code=201)
+def register_patient(patient: Patient):
+	if patient.id is None:
+		patient.id = app.patient_id
+		app.patient_id +=1
+
+	r_date = datetime.date.today()
+	sr_date = r_date.strftime("%Y-%m-%d")
+	patient.register_date = sr_date
+	
+	ns_length = len(patient.name.replace(" ", ""))+len(patient.surname.replace(" ", ""))
+	date_delta = timedelta(days=+ns_length)
+	vac_date = r_date + date_delta
+	s_vac_date = vac_date.strftime("%Y-%m-%d")
+	patient.vaccination_date = s_vac_date
+
+	return patient 
