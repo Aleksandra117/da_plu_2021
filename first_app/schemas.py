756e44e167adbfca369b1c27905294155d404fc7
Aleksandra117 <ola.adamska10@gmail.com>
2021-05-19 00:27:47 +0200

    Task 5.3v2

--- first_app/schemas.py
+++ first_app/schemas.py
@@ -47,8 +47,6 @@ class SupplierCreate(BaseModel):
     PostalCode: Optional[constr(max_length=10)]
     Country: Optional[constr(max_length=15)]
     Phone: Optional[constr(max_length=24)]
-    Fax: Optional[constr(max_length=24)] = None
-    HomePage: Optional[str] = None
     class Config:
         orm_mode = True
 
