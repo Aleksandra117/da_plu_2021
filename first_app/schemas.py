b0a4191232c90add658ee85b5fe9422168b18c98
Aleksandra117 <ola.adamska10@gmail.com>
2021-05-19 02:32:38 +0200

    Task 5.3v4

--- first_app/schemas.py
+++ first_app/schemas.py
@@ -21,15 +21,15 @@ class Supplier(BaseModel):
 
 class SupplierExtended(BaseModel): 
     SupplierID: PositiveInt
-    CompanyName: constr(max_length=40)
-    ContactName: constr(max_length=30)
-    ContactTitle: constr(max_length=30)
-    Address: constr(max_length=60)
-    City: constr(max_length=15)
+    CompanyName: Optional[constr(max_length=40)]
+    ContactName: Optional[constr(max_length=30)]
+    ContactTitle: Optional[constr(max_length=30)]
+    Address: Optional[constr(max_length=60)]
+    City: Optional[constr(max_length=15)]
     Region: Optional[constr(max_length=15)]
-    PostalCode: constr(max_length=10)
-    Country: constr(max_length=15)
-    Phone: constr(max_length=24)
+    PostalCode: Optional[constr(max_length=10)]
+    Country: Optional[constr(max_length=15)]
+    Phone: Optional[constr(max_length=24)]
     Fax: Optional[constr(max_length=24)] = None
     HomePage: Optional[str] = None
     class Config:
